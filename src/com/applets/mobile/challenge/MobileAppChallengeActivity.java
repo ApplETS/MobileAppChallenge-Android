fdad4ad7c4bc1f126a6fcdc12a553439c31111bf
Philippe David <philippe.david0@gmail.com>
2012-02-15 18:24:29 -0500

    empty android project

---
+++ src/com/applets/mobile/challenge/MobileAppChallengeActivity.java
@@ -0,0 +1,13 @@
+package com.applets.mobile.challenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobileAppChallengeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
