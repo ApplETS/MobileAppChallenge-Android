99109f2ef3d70466049e4d78dbd4ed88b4c126ce
Michael Bernier <infosonic@gmail.com>
2012-03-16 10:58:18 -0400

    ajout on long item click listener pour les chansons

--- src/com/applets/mobile/challenge/BasicListActivity.java
+++ src/com/applets/mobile/challenge/BasicListActivity.java
@@ -2,12 +2,16 @@ package com.applets.mobile.challenge;
 
 import org.json.JSONObject;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 
 import com.applets.mobile.challenge.adapters.ArtistAdapter;
@@ -15,50 +19,84 @@ import com.applets.mobile.challenge.utils.AdapterFactory;
 import com.applets.mobile.challenge.utils.IAsyncTaskListener;
 import com.applets.mobile.challenge.utils.JSONRetreiver;
 
-//TODO on long click for songs
 public class BasicListActivity extends ListActivity implements
-	IAsyncTaskListener {
+		IAsyncTaskListener {
 
-    private String query;
-    private String type;
-    // Handles the onPostExecute
-    private final Handler handler = new Handler();
+	private String query;
+	private String type;
+	// Handles the onPostExecute
+	private final Handler handler = new Handler();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.basic_list);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.basic_list);
 
 		Bundle bundle = getIntent().getExtras();
 		query = bundle.getString("query");
 		type = bundle.getString("type");
-		
+
 		if (type.equals("songs")) {
-			new JSONRetreiver(this).execute("http://highwizard.com:8080/list", query);
+			new JSONRetreiver(this).execute("http://highwizard.com:8080/list",
+					query);
 		} else if (type.equals("albums")) {
-			new JSONRetreiver(this).execute("http://highwizard.com:8080/list", query);
-		} else if (type.equals("artist")){
+			new JSONRetreiver(this).execute("http://highwizard.com:8080/list",
+					query);
+		} else if (type.equals("artist")) {
 			new JSONRetreiver(this).execute("http://highwizard.com:8080/list");
 		}
-		
+
+		this.getListView().setOnItemLongClickListener(new LongClickListener());
+
 	}
 
-    @Override
-    public void onPostExecute(final JSONObject result) {
-	final Context ctx = this;
-	handler.post(new Runnable() {
+	private class LongClickListener implements OnItemLongClickListener {
 
-	    @Override
-	    public void run() {
-		setListAdapter(AdapterFactory.getInstance().getAdapter(type,
-			ctx, result));
-	    }
-	});
-    }
+		@Override
+		public boolean onItemLongClick(AdapterView<?> adapterViews, View view,
+				int position, long id) {
+
+			if (type.equals("songs")) {
+				final CharSequence[] items = { "Add song to playlist",
+						"Play now" };
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						view.getContext());
+				builder.setTitle("Choose an option");
+				builder.setItems(items, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int item) {
+						if (item == 0) {
+
+						} else if (item == 1) {
+
+						}
+					}
+				});
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+			return false;
+		}
+	}
 
 	@Override
-	protected void onListItemClick(ListView listView, View view, int position, long id) {
-		String text = ((ArtistAdapter) listView.getAdapter()).getLabel(position);
+	public void onPostExecute(final JSONObject result) {
+		final Context ctx = this;
+		handler.post(new Runnable() {
+
+			@Override
+			public void run() {
+				setListAdapter(AdapterFactory.getInstance().getAdapter(type,
+						ctx, result));
+			}
+		});
+	}
+
+	@Override
+	protected void onListItemClick(ListView listView, View view, int position,
+			long id) {
+		String text = ((ArtistAdapter) listView.getAdapter())
+				.getLabel(position);
 		Intent intent = null;
 		if (type.equals("artist")) {
 			intent = new Intent(this, BasicListActivity.class);
@@ -69,10 +107,10 @@ public class BasicListActivity extends ListActivity implements
 			intent.putExtra("type", "songs");
 			intent.putExtra("query", query + text);
 		} else {
-			//TODO if for songs
+			// TODO if for songs
 			return;
 		}
-		
+
 		startActivity(intent);
-    }
+	}
 }
